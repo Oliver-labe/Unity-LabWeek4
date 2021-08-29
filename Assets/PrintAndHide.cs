30e3d4a0d4adaecd4f9c53ed83373ede6cc711f1
Qianhao Liu <liuqianhao6@gmail.com>
2021-08-29 13:53:48 +0800

    90% percent complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour {
+public class PrintAndHide : MonoBehaviour {
     public Renderer rend;
     int i, randValue;
 
